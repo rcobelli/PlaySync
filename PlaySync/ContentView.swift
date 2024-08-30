d71e0d015b66f3c7444d45144ca4c8fbe09bb802
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2024-08-30 18:10:42 -0400

    Initial Commit

---
+++ PlaySync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlaySync
+//
+//  Created by Ryan Cobelli on 8/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlaySync/PlaySyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaySyncApp.swift
+//  PlaySync
+//
+//  Created by Ryan Cobelli on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaySyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
